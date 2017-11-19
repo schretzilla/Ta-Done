0209d047be8d45976285ad665109c37313049b1d
schretzilla <schretzilla@gmail.com>
2017-11-19 07:29:38 -0800

    added routing to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
+// Routing
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ProjectDetailComponent } from './project/project-detail.component';
 import { ProjectComponent } from './project/project.component';
@@ -9,6 +12,11 @@ import { ToDoComponent } from './to-do/to-do.component';
 
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +28,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    RouterModule.forRoot(   //TODO split into it's own routing module file
+      appRoutes,
+      { enableTracing: true } //for debugging purposes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
